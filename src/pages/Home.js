52196eaa9e95460d2c8ee3aa212d68e58fa474a8
Abhinandan Darbey <tiger@pop-os.localdomain>
2021-05-31 17:49:39 +0530

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import {Grid, Button, Typography} from '@material-ui/core';
+import Icon from '@mdi/react'
+import { mdiQrcodeScan } from '@mdi/js';
+import { Link } from "react-router-dom";
+
+function Home() {
+
+    return (
+        <div>
+
+            <Typography style={{margin:30}} variant="h2">
+            ScanCode 
+            </Typography>
+
+            <Grid container spacing={6}>
+                <Grid item xs={6}>
+                    <Link to="/qr_scanner">
+                    <Button variant="contained" size="large" color="primary">
+                        <Icon 
+                        style={{padding:10}}
+                        path={mdiQrcodeScan}
+                        title="QR Scanner"
+                        size={15}
+                        color="white"
+                        />
+                    </Button>
+                    </Link>
+                </Grid>
+            </Grid>
+        
+        </div>
+    );
+  }
+  
+  export default Home;
+  
