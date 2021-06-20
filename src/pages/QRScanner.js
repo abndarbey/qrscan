9107430d3b33c20cdde01bc8e43da67d778397d8
Abhinandan Darbey <abhinandan.darbey@gmail.com>
2021-06-20 20:10:58 +0530

    open link in the same tab

--- src/pages/QRScanner.js
+++ src/pages/QRScanner.js
@@ -15,7 +15,7 @@ export const Scanner = () => {
     }
     if(link!=="No result"){
         let newLink = link + '?productID=' + productID 
-        window.open(newLink, "_blank");
+        window.open(newLink);
     }
     const handleError = err => {
         console.error(err)
